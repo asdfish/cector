e9a17c278aecf6533aa5c74d42f0bd7fdbfc0755
asdfish <lgray3420@gmail.com>
2024-10-31 15:42:52 -0400

    added push function

--- cector.h
+++ cector.h
@@ -26,14 +26,14 @@
     size_t reserved;\
   };\
   \
-  extern exit_type at_function_name(struct struct_name*, type** output);\
-  extern exit_type init_function_name(struct struct_name*);\
-  extern exit_type length_function_name(struct struct_name*, unsigned int* length);\
-  extern exit_type pop_function_name(struct struct_name*);\
-  extern exit_type push_function_name(struct struct_name*);\
-  extern exit_type realloc_contents_function_name(struct struct_name*, unsigned int suggested_size);\
-  extern exit_type reserve_function_name(struct struct_name*);\
-  extern exit_type uninit_function_name(struct struct_name*);
+  extern exit_type at_function_name(struct struct_name* vector, unsigned int index, type** output);\
+  extern exit_type init_function_name(struct struct_name* vector);\
+  extern exit_type length_function_name(struct struct_name* vector, unsigned int* length);\
+  extern exit_type pop_function_name(struct struct_name* vector);\
+  extern exit_type push_function_name(struct struct_name* vector, type item);\
+  extern exit_type realloc_contents_function_name(struct struct_name* vector, unsigned int suggested_size);\
+  extern exit_type reserve_function_name(struct struct_name* vector, unsigned int reservation);\
+  extern exit_type uninit_function_name(struct struct_name* vector);
 #endif
 
 // generates function source code for .c files
@@ -52,6 +52,15 @@
     realloc_contents_function_name,\
     reserve_function_name,\
     uninit_function_name)\
+  \
+  exit_type at_function_name(struct struct_name* vector, unsigned int index, type** output) {\
+    if(vector == NULL || output == NULL)\
+      return failure;\
+    \
+    *output = *(vector->contents + index);\
+    \
+    return success;\
+  }\
   \
   exit_type init_function_name(struct struct_name* vector) {\
     if(vector == NULL)\
@@ -78,6 +87,87 @@
       output_length ++;\
     \
     *length = output_length;\
+    \
+    return success;\
+  }\
+  \
+  exit_type push_function_name(struct struct_name* vector, type item) {\
+    if(vector == NULL)\
+      return failure;\
+    \
+    type* new_item = (type*) malloc(sizeof(type));\
+    if(new_item == NULL)\
+      return failure;\
+    *new_item = item;\
+    \
+    unsigned int length = 0;\
+    if(length_function_name(vector, &length) != success)\
+      goto free_new_item;\
+    \
+    if(realloc_contents_function_name(vector, length + 1) != success)\
+      goto free_new_item;\
+    \
+    *(vector->contents + length) = new_item;\
+    *(vector->contents + length + 1) = NULL;\
+    \
+    return success;\
+    \
+free_new_item:\
+    free(new_item);\
+    new_item = NULL;\
+    return failure;\
+  }\
+  \
+  exit_type pop_function_name(struct struct_name* vector) {\
+    if(vector == NULL)\
+      return failure;\
+    \
+    unsigned int length = 0;\
+    if(length_function_name(vector, &length) != success)\
+      return failure;\
+    \
+    if(length == 0)\
+      return success;\
+    \
+    length --;\
+    \
+    realloc_contents_function_name(vector, length);\
+    \
+    return success;\
+  }\
+  \
+  exit_type realloc_contents_function_name(struct struct_name* vector, unsigned int suggested_size) {\
+    if(vector == NULL ||\
+        (suggested_size <= vector->reserved && vector->size == vector->reserved))\
+      return failure;\
+    \
+    unsigned int new_size = suggested_size <= vector->reserved ? vector->reserved : suggested_size;\
+    \
+    unsigned int length = 0;\
+    if(length_function_name(vector, &length) != success)\
+      return failure;\
+    \
+    if(new_size < length)\
+      for(unsigned int i = new_size; i < length; i ++) {\
+        free(*(vector->contents + length));\
+        *(vector->contents + length) = NULL;\
+      }\
+    \
+    type** temp_pointer = realloc(vector->contents, (new_size + 1) * sizeof(type*));\
+    if(temp_pointer == NULL)\
+      return failure;\
+    \
+    vector->contents = temp_pointer;\
+    vector->size = new_size;\
+    \
+    return success;\
+  }\
+  \
+  exit_type reserve_function_name(struct struct_name* vector, unsigned int reservation) {\
+    if(vector == NULL)\
+      return failure;\
+    \
+    vector->reserved = reservation;\
     \
     return success;\
   }\
--- tests/src/main.c
+++ tests/src/main.c
@@ -1,4 +1,26 @@
 #include <int_vector.h>
+#include <stdio.h>
 int main(void) {
+  struct int_vector vector;
+  if(int_vector_init(&vector) != 0)
+    return -1;
+
+  int_vector_push(&vector, 69);
+
+  unsigned int length = 0;
+  if(int_vector_length(&vector, &length) != 0)
+    goto cleanup;
+
+  printf("length: %u\n", length);
+
+  int* my_int = NULL;
+  if(int_vector_at(&vector, 0, &my_int) != 0)
+    goto cleanup;
+  printf("[0]: %i\n", *my_int);
+
+  /*int_vector_pop(&vector);*/
+
+cleanup:
+  int_vector_uninit(&vector);
   return 0;
 }
